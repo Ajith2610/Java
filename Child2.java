d79aef426d53bf8951c7a90221ae71ef6cdd2593
Ajith2610 <135815075+Ajith2610@users.noreply.github.com>
2023-06-07 16:26:12 +0530

    Add files via upload

---
+++ Child2.java
@@ -0,0 +1,34 @@
+package p4;
+
+
+
+public class Child2 
+
+{
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Child2 ch=new Child2();
+		ch.play();
+		ch.read();
+		
+		System.out.println();
+
+		
+
+	}
+	public void play()
+	{
+		System.out.println("playing");
+	}
+
+	public void read()
+	{
+		System.out.println("reading");
+	}
+
+
+
+	}
+	
